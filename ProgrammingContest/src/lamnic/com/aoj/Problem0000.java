8b2b567ce1f61c825cb1e0e9feddb3f559478441
Masa <Masa@OPC12546088055>
2016-06-06 06:38:15 +0900

    フォーマッタを適用

--- ProgrammingContest/src/lamnic/com/aoj/Problem0000.java
+++ ProgrammingContest/src/lamnic/com/aoj/Problem0000.java
@@ -1,10 +1,12 @@
 package lamnic.com.aoj;
 
 public class Problem0000 {
-	public static void solve(){
-		for(int i = 1;i<10;i++){
-			for(int n = 1;n<10;n++){
-				System.out.println(String.format("%dx%d=%d",i,n,i*n));
+	private static final int MAX_VAL = 10;
+
+	public static void solve() {
+		for (int i = 1; i < MAX_VAL; i++) {
+			for (int n = 1; n < MAX_VAL; n++) {
+				System.out.println(String.format("%dx%d=%d", i, n, i * n));
 			}
 		}
 	}

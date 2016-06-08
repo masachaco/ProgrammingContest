e9365e4bdc62c84fc79d8a217a6dac19bca8bd51
Masa <Masa@192.168.2.102>
2016-06-09 00:37:24 +0900

    Problem0002の回答を追加

--- ProgrammingContest/src/lamnic/com/aoj/Problem0002.java
+++ ProgrammingContest/src/lamnic/com/aoj/Problem0002.java
@@ -1,12 +1,23 @@
-package lamnic.com;
+package lamnic.com.aoj;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Problem0002 {
 	public static void main(String[] args) {
 		// List<String> sampleInput = SampleInputReader.read("0002", "sample1");
+		List<String> sampleInput = getInput();
+		sampleInput.stream().forEach(d -> solve(d));
+	}
+
+	private static void solve(String d) {
+		String[] split = d.split(" ");
+		System.out.println(String.format("%d", toLong(split[0]) + toLong(split[1])).length());
+	}
+
+	private static long toLong(String token) {
+		return Long.parseLong(token);
 	}
 
 	/*
